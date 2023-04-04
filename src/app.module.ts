8215759bf2289425b569abae95a143b3baea2455
makkahwi <suhaibahmadai@hotmail.com>
2023-04-04 15:01:36 +0300

    Add Families Module To App Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './config/data-source';
+import { FamiliesModule } from './families/families.module';
 import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     UsersModule,
+    FamiliesModule,
     RolesModule,
     TypeOrmModule.forRoot({
       ...config,
