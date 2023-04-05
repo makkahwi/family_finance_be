2b3de1ef233e69aaae29dd286332acd9eea56931
makkahwi <suhaibahmadai@hotmail.com>
2023-04-05 14:36:05 +0300

    Add Requests Validation Infrastructure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   const config = new DocumentBuilder()
     .setTitle('App APIs')
