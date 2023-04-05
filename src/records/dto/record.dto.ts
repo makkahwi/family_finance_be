c301d3f46af0b96708040a3b99eae4399de8b23f
makkahwi <suhaibahmadai@hotmail.com>
2023-04-05 15:01:22 +0300

    Add Record Validations

--- src/records/dto/record.dto.ts
+++ src/records/dto/record.dto.ts
@@ -1,5 +1,16 @@
+import { IsInt, IsNotEmpty, Length, Min } from 'class-validator';
+
 export class RecordDto {
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
   value: number;
+
+  @IsNotEmpty()
+  @Length(3, 30)
   note: string;
+
+  @IsNotEmpty()
+  @Length(10, 20)
   timestamp: string;
 }
