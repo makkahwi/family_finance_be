0de8b34e4f0f6e414b48e40d5ca0a3554f1a5a1b
makkahwi <suhaibahmadai@hotmail.com>
2023-04-05 15:01:01 +0300

    Add Account Validations

--- src/accounts/dto/account.dto.ts
+++ src/accounts/dto/account.dto.ts
@@ -1,5 +1,15 @@
+import { IsInt, IsNotEmpty, Length, Min } from 'class-validator';
+
 export class AccountDto {
+  @IsNotEmpty()
+  @Length(3, 20)
   name: string;
+
+  @IsNotEmpty()
+  @Length(3, 20)
   currency: string;
+
+  @IsNotEmpty()
+  @Length(3, 50)
   description: string;
 }
