746e2ee24ee011fc075ac0c0a798aa8cf3771e75
makkahwi <suhaibahmadai@hotmail.com>
2023-04-05 15:01:34 +0300

    Add Transfer Validations

--- src/transfers/dto/transfer.dto.ts
+++ src/transfers/dto/transfer.dto.ts
@@ -1,5 +1,12 @@
+import { IsInt, IsNotEmpty, Length, Min } from 'class-validator';
+
 export class TransferDto {
-  name: string;
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0.1)
   amount: number;
+
+  @IsNotEmpty()
+  @IsInt()
   exchangeRate: number;
 }
