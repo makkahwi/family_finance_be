f3412166076ce80e4d270598a50756265dd963f8
makkahwi <suhaibahmadai@hotmail.com>
2023-04-16 10:40:07 +0300

    Turn DB To SqlLite

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,20 +1,10 @@
 import { DataSourceOptions } from 'typeorm';
 
-import { User } from '../users/entities/user.entity';
-
 import 'reflect-metadata';
 
 export const config: DataSourceOptions = {
-  type: 'postgres',
-  host: process.env.DB_HOST || '127.0.0.1',
-  port: parseInt(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'postgres',
+  type: 'sqlite',
+  database: 'sqlite',
+  entities: [__dirname + '/**/data/*.entity{.ts,.js}'],
   synchronize: true,
-  logging: false,
-  // entities: [User],
-  // entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  migrations: ['src/migration/*.ts'],
-  subscribers: [],
 };
